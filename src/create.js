ac5dc06fecc4c5bac7cb79230a81690579045a60
Francesco Furlan <francesco.furlan@molo17.com>
2020-03-25 03:00:08 +0100

    feat: insert data into couchbase on create

--- src/create.js
+++ src/create.js
@@ -3,12 +3,38 @@
 const { v4 } = require("uuid");
 const {
   successfullHttpResult,
+  internalErrorHttpResult,
   invalidParameterHttpResult
 } = require("./httpResultUtils");
+const { resolve } = require("path");
+const couchbase = require("couchbase");
 
 const EMAIL_REGEX = /^[\w-.]+@[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}$/i;
 
-const createHandler = async (event, _context) => {
+const {
+  CLUSTER_USERNAME,
+  CLUSTER_PASSWORD,
+  CLUSTER_CONNECTION_STRING,
+  BUCKET
+} = process.env;
+
+const certpath = resolve(process.cwd(), "secrets/ca.pem");
+
+const cluster = new couchbase.Cluster(
+  `${CLUSTER_CONNECTION_STRING}?certpath=${certpath}`
+);
+
+cluster.authenticate(CLUSTER_USERNAME, CLUSTER_PASSWORD);
+
+const bucket = cluster.openBucket(BUCKET);
+
+bucket.on("error", error => {
+  console.log("Error from bucket:", JSON.stringify(error, null, 2));
+});
+
+const createHandler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
   const requestBody = JSON.parse(event.body);
   const { id = v4(), name = "", surname = "", email = "" } = requestBody;
 
@@ -35,7 +61,26 @@ const createHandler = async (event, _context) => {
     type: "Contact"
   };
 
-  return successfullHttpResult(document);
+  const documentId = `contact::${id}`;
+
+  try {
+    const documentInserted = await new Promise((resolve, reject) => {
+      bucket.insert(documentId, document, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ ...result, ...document });
+        }
+      });
+    });
+    console.log("Remaining time: ", context.getRemainingTimeInMillis());
+    return successfullHttpResult(documentInserted);
+  } catch (error) {
+    const errorId = v4();
+    console.log(`Error ${errorId}`, JSON.stringify(error, null, 2));
+    console.log("Remaining time: ", context.getRemainingTimeInMillis());
+    return internalErrorHttpResult(errorId);
+  }
 };
 
 module.exports.default = createHandler;
