69daa3b84cca04b25a2ceaf8d3c755e5cf13d7e2
Francesco Furlan <francesco.furlan@molo17.com>
2020-03-25 11:08:09 +0100

    feat: add contactId, body parsing and validation

--- src/update.js
+++ src/update.js
@@ -1,14 +1,48 @@
-const updateHandler = async (_event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Hello from update function!"
-      },
-      null,
-      2
-    )
+"use strict";
+const {
+  successfullHttpResult,
+  invalidParameterHttpResult
+} = require("./httpResultUtils");
+
+const EMAIL_REGEX = /^[\w-.]+@[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}$/i;
+
+const updateHandler = async (event, _context) => {
+  const {
+    pathParameters: { contactId }
+  } = event;
+
+  if (!event.body || typeof event.body !== "string") {
+    return invalidParameterHttpResult("body");
+  }
+
+  const requestBody = JSON.parse(event.body);
+
+  const { name = "", surname = "", email = "" } = requestBody;
+
+  if (typeof name !== "string" || !name.length || name.length > 64) {
+    return invalidParameterHttpResult("name");
+  }
+
+  if (typeof surname !== "string" || !surname.length || surname.length > 64) {
+    return invalidParameterHttpResult("surname");
+  }
+
+  if (
+    typeof email !== "string" ||
+    (email.length && (email.length > 64 || !EMAIL_REGEX.test(email)))
+  ) {
+    return invalidParameterHttpResult("email");
+  }
+
+  const document = {
+    id: contactId,
+    name,
+    surname,
+    email,
+    type: "Contact"
   };
+
+  return successfullHttpResult(document);
 };
 
 module.exports.default = updateHandler;
