4a79869806e49ff884158d594624082ad7ea86b3
Francesco Furlan <francesco.furlan@molo17.com>
2020-03-25 12:04:51 +0100

    feat: add update contact doc on update function

--- src/update.js
+++ src/update.js
@@ -1,12 +1,40 @@
 "use strict";
+
 const {
   successfullHttpResult,
+  internalErrorHttpResult,
   invalidParameterHttpResult
 } = require("./httpResultUtils");
+const { v4 } = require("uuid");
+const { resolve } = require("path");
+const couchbase = require("couchbase");
 
 const EMAIL_REGEX = /^[\w-.]+@[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,}$/i;
 
-const updateHandler = async (event, _context) => {
+const {
+  CLUSTER_USERNAME,
+  CLUSTER_PASSWORD,
+  CLUSTER_CONNECTION_STRING,
+  BUCKET
+} = process.env;
+
+const certpath = resolve(process.cwd(), "secrets/ca.pem");
+
+const cluster = new couchbase.Cluster(
+  `${CLUSTER_CONNECTION_STRING}?certpath=${certpath}`
+);
+
+cluster.authenticate(CLUSTER_USERNAME, CLUSTER_PASSWORD);
+
+const bucket = cluster.openBucket(BUCKET);
+
+bucket.on("error", error => {
+  console.log("Error from bucket:", JSON.stringify(error, null, 2));
+});
+
+const updateHandler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
   const {
     pathParameters: { contactId }
   } = event;
@@ -42,7 +70,26 @@ const updateHandler = async (event, _context) => {
     type: "Contact"
   };
 
-  return successfullHttpResult(document);
+  const documentId = `contact::${contactId}`;
+
+  try {
+    const documentInserted = await new Promise((resolve, reject) => {
+      bucket.upsert(documentId, document, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve({ ...result, ...document });
+        }
+      });
+    });
+
+    return successfullHttpResult(documentInserted);
+  } catch (error) {
+    const errorId = v4();
+    console.log(`Error ${errorId}`, JSON.stringify(error, null, 2));
+
+    return internalErrorHttpResult(errorId);
+  }
 };
 
 module.exports.default = updateHandler;
